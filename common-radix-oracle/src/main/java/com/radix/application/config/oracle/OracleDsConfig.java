f26a12ca3f02daa4dcb6ccd244485c5a8fae5916
Ricardolv <richardluizv@gmail.com>
2021-01-04 12:31:22 -0300

    adicinando anotacao @Profile  deferente de test para nao conflitar na configuracao de teste

--- common-radix-oracle/src/main/java/com/radix/application/config/oracle/OracleDsConfig.java
+++ common-radix-oracle/src/main/java/com/radix/application/config/oracle/OracleDsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -17,6 +18,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.HashMap;
 
+@Profile("!test")
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(
