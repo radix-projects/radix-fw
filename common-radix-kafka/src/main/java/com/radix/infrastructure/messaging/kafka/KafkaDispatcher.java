22be8c40cdfb0c5bd2452cb1f0b3cf176736e55b
Leonardo Andreatta de Alcantara <alcantaraleo@gmail.com>
2021-05-05 17:21:51 -0300

    Correção KafkaDispatcher
    Removida a chamada para kafkaTemplate.flush() no callback de sucesso de envio de mensagem. Ao fazer a chamada a produção de novas mensagens eram interrompidas. Adicionado método preDestroy para chamar o flush() somente no momento de desligamento do serviço. O Kafka por padrão aguarda 5 segundos para dar flush nos buffers, o flush()  é útil para, quando o serviço desliga, enviar as últimas mensagens que ainda estejam no buffer.

--- common-radix-kafka/src/main/java/com/radix/infrastructure/messaging/kafka/KafkaDispatcher.java
+++ common-radix-kafka/src/main/java/com/radix/infrastructure/messaging/kafka/KafkaDispatcher.java
@@ -1,50 +1,59 @@
 package com.radix.infrastructure.messaging.kafka;
 
-import com.radix.infrastructure.messaging.kafka.config.producer.KafkaProducerConfig;
+import lombok.SneakyThrows;
 import lombok.extern.java.Log;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import javax.annotation.PreDestroy;
 import java.util.Objects;
 
 @Log
 @Component
 public class KafkaDispatcher<T> {
 
-    @Autowired
-    private KafkaTemplate<String, Message<T>> kafkaTemplate;
+	@Autowired
+	private KafkaTemplate<String, Message<T>> kafkaTemplate;
 
-    public void send(String topic, String key, CorrelationId correlationId, T payLoad) {
+	@SneakyThrows public void send(String topic, String key, CorrelationId correlationId, T payLoad) {
 
-        if (Objects.nonNull(correlationId)) {
-            correlationId.continueWith("_" + topic);
-        }
+		if (Objects.nonNull(correlationId)) {
+			correlationId.continueWith("_" + topic);
+		}
 
-        Message<T> message = new Message<>(correlationId, payLoad);
+		Message<T> message = new Message<>(correlationId, payLoad);
 
-        log.info(String.format("Sending : %s", message));
-        log.info("--------------------------------");
+		log.info(String.format("Sending : %s", message));
+		log.info("--------------------------------");
 
-        var future = kafkaTemplate.send(topic, key, message);
+		var future = kafkaTemplate.send(topic, key, message);
 
-        future.addCallback(new ListenableFutureCallback<>() {
-            @Override
-            public void onSuccess(SendResult<String, Message<T>> result) {
-                log.info("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
-                kafkaTemplate.flush();
-            }
+		future.addCallback(new ListenableFutureCallback<>() {
 
-            @Override
-            public void onFailure(Throwable ex) {
-                log.warning("Unable to send message=[" + message + "] due to : " + ex.getMessage());
-            }
-        });
-    }
+			@Override
+			public void onSuccess(SendResult<String, Message<T>> result) {
+
+				log.info("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+			}
+
+			@Override
+			public void onFailure(Throwable ex) {
+
+				log.warning("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+			}
+
+		});
+
+	}
+
+	@PreDestroy
+	public void flush() {
+
+		this.kafkaTemplate.flush();
+	}
 
 }
 
